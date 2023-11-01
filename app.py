b633209f7333aacdece3c597ecf09721ac534671
IuliaVrabie <57674096+IuliaVrabie@users.noreply.github.com>
2023-11-01 09:40:06 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template
 import numpy as np
 
 app = Flask(__name__)
@@ -14,13 +14,15 @@ def update_grid(grid):
     new_grid = np.copy(grid)
     for i in range(grid_size[0]):
         for j in range(grid_size[1]):
-            neighborhood = grid[i - 1:i + 2, j - 1:j + 2]
-            alive_neighbors = np.sum(neighborhood) - grid[i, j]
+            neighborhood = grid[i - 1:i + 2, j - 1:j + 2]  # if cell is (1,1) then grid[0,1,2 : 0,1,2]
+            neighbors = np.sum(neighborhood)  - grid[i, j]
             if grid[i, j] == 1:
-                if alive_neighbors <= 1 or alive_neighbors >= 4:
+                # Death Loneliness/ Overpopulation
+                if neighbors <= 1 or neighbors >= 4:
                     new_grid[i, j] = 0
             else:
-                if alive_neighbors == 3:
+                # Birth
+                if neighbors == 3:
                     new_grid[i, j] = 1
     return new_grid
 
@@ -28,10 +30,12 @@ def update_grid(grid):
 def grid_to_list(grid):
     grid_list = []
     for row in grid:
-        row_list = [str(cell) for cell in row]
+        row_list = []
+        for cell in row:
+            cell_str = str(cell)
+            row_list.append(cell_str)
         grid_list.append(row_list)
     return grid_list
-
 
 # Route to display the grid as an HTML table
 @app.route('/')
@@ -43,14 +47,16 @@ def display_grid():
 def get_grid():
     global grid
     grid = update_grid(grid)  # Update the grid
+    print(f"grid {grid}")
     grid_list = grid_to_list(grid)
-    return jsonify(result=grid_list)
+    print(f"grid_list {grid_list}")
+    return {'result': grid_list}
 
 # Function to randomize the grid with a new random configuration
 def randomize_grid():
     global grid
     grid = np.random.choice([0, 1], size=grid_size)
-    return jsonify(result="Randomized")
+    return {'result': 'Randomized'}
 
 # Route to randomize the grid
 @app.route('/randomize_grid')
